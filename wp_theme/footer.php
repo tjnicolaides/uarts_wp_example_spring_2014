d970a4b752906434b87986e33439b713c618fd52
TJ Nicolaides <tjnicolaides@gmail.com>
2014-02-15 13:43:52 -0500

    splitting index up into header, footer, and sidebar files

---
+++ wp_theme/footer.php
@@ -0,0 +1,16 @@
+
+    <div class="blog-footer">
+      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+
+  </body>
+</html>
