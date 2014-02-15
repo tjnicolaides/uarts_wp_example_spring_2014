ef09e98c68e6ebe70fbe4a25f37fc3c9b1c9e3f1
TJ Nicolaides <tjnicolaides@gmail.com>
2014-02-15 14:25:46 -0500

    new single and page templates

--- wp_theme/page.php
+++ wp_theme/page.php
@@ -4,13 +4,13 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-8 blog-main">
-        
+        	<h1>Page</h1>
 			<?php while ( have_posts() ) : the_post() ?>
 				<! -- this is our post / page content START --> 
 				<div class="blog-post">
 					<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 					<p class="blog-post-meta"><?php the_time('Y-m-d') ?> by <a href="#"><?php the_author(); ?></a></p>
-					<?php the_excerpt(); ?>
+					<?php the_content(); ?>
 				
 				</div>
 				<! -- this is our post / page content END --> 
